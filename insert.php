481001c6e140499e54061dcd808c52faa8766ca9
Alan De Yiber Hernandez Salguero <alan.hernandez@ucp.edu.co>
2023-01-28 20:36:59 -0500

    insert funcional

--- insert.php
+++ insert.php
@@ -6,25 +6,35 @@ include_once 'class/Connection/connection.php';
 
 $json = file_get_contents('php://input');
 $params = json_decode($json);
+//return var_dump($params);
 class insert {
   
     private $params;
+    private $nombre;
+    private $correo;
+    private $contrasena;
+
+
     public function __construct($params) {
-        $this->params = $params;
+        $this->nombre = $params->nombre;
+        $this->correo = $params->correo;
+        $this->contrasena = $params->contrasena;
     }
    
     public function postData (){
-        
+        //return var_dump($this->params);
         $conn = new connection;
         $conn->connection->query("insert into admin (nombre, correo, contrasena) 
-        values ('$this->params->nombre','$this->params->correo','$this->params->contrasena')");
+        values ('$this->nombre','$this->correo','$this->contrasena')")or die("Problemas en el selct: " . mysqli_error($conn->connection));
 
+        return 1;
+                
     }
 
 }
 
 $inse = new insert($params);
 
-$inse->postData();
+echo($inse->postData());
 
 ?>
